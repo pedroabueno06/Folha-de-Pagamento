63b6059f1f02d318d7417265e389134718d1ec56
pedroabueno06 <pedro.ascenco@gmail.com>
2026-09-24 20:33:33 -0300

    Atualização

--- src/main/java/com/example/folhaPagamento/view/Cadastro.java
+++ src/main/java/com/example/folhaPagamento/view/Cadastro.java
@@ -2,15 +2,18 @@ package com.example.folhaPagamento.view;
 
 import javax.swing.JOptionPane;
 
+import com.example.folhaPagamento.model.Colaborador;
 import com.example.folhaPagamento.repositorio.GerenciadorColaboradores;
+
 public class Cadastro {
 
     private GerenciadorColaboradores gerenciador;
-    public Cadastro (GerenciadorColaboradores gerenciador) {
+
+    public Cadastro(GerenciadorColaboradores gerenciador) {
         this.gerenciador = gerenciador;
     }
 
-     public void iniciar() {
+    public void iniciar() {
         boolean continuar = true;
 
         while (continuar) {
@@ -37,34 +40,34 @@ public class Cadastro {
     }
 
     private int exibirMenu() {
-        Object[]  opcoes = {"Cadastrar Colaborador",
-                            "Listar Colaboradores",
-                            "Atualizar Colaborador",
-                            "Remover Colaborador",
-                            "Gerar Folha de Pagamento",
-                            "Sair"};
+        Object[] opcoes = {"Cadastrar Colaborador",
+            "Listar Colaboradores",
+            "Atualizar Colaborador",
+            "Remover Colaborador",
+            "Gerar Folha de Pagamento",
+            "Sair"};
 
-            return JOptionPane.showOptionDialog (null,
-            "Selecione a opção desejada:",          //Mensagem de entrada
-            "======================================================= FOLHA DE PAGAMENTO ===================================================", //Título da janela
-            JOptionPane.DEFAULT_OPTION,                      //Tipo de Opção
-            JOptionPane.QUESTION_MESSAGE,
-            null,
-            opcoes,                                          //Botões em ordem
-            opcoes[0]);
+        return JOptionPane.showOptionDialog(null,
+                "Selecione a opção desejada:", //Mensagem de entrada
+                "======================================================= FOLHA DE PAGAMENTO ===================================================", //Título da janela
+                JOptionPane.DEFAULT_OPTION, //Tipo de Opção
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                opcoes, //Botões em ordem
+                opcoes[0]);
     }
 
     private String lerTipoColaborador() {
         String[] opcoes = {"Padrão", "Comissionado", "Produção"};
 
         String tipoEscolhido = (String) JOptionPane.showInputDialog(
-            null,
-                    "Selecione o tipo de colaborador: ",
-                      "Tipo de colaborador: ",
-                            JOptionPane.QUESTION_MESSAGE,
-                            null,
-                            opcoes,
-                            opcoes[0]
+                null,
+                "Selecione o tipo de colaborador: ",
+                "Tipo de colaborador: ",
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                opcoes,
+                opcoes[0]
         );
 
         return tipoEscolhido;
@@ -77,23 +80,22 @@ public class Cadastro {
         //Loop de para ver se o usuário digitou um nome válido
         while (!nomeValido) {
             nomeCompleto = JOptionPane.showInputDialog(null, "Digite o nome do colaborador:",
-            "Validação de Nome", JOptionPane.QUESTION_MESSAGE);
+                    "Validação de Nome", JOptionPane.QUESTION_MESSAGE);
 
             //Verifica se o usuário encerrou o programa.
             if (nomeCompleto == null) {
-                return null; 
+                return null;
             }
 
             //Faz a remoção de espaços em branco que podem estar no início ou no fim do nome digitado.
             nomeCompleto = nomeCompleto.trim();
-
 
             if (!nomeCompleto.isEmpty() && nomeCompleto.length() >= 2) {
                 nomeValido = true;
 
             } else {
                 JOptionPane.showMessageDialog(null, "Nome Inválido! Seu nome não deve estar em branco, além de ter que informar seu nome completo.",
-                "Nome Inválido", JOptionPane.ERROR_MESSAGE);
+                        "Nome Inválido", JOptionPane.ERROR_MESSAGE);
             }
         }
 
@@ -101,16 +103,16 @@ public class Cadastro {
     }
 
     private Integer lerMatricula() {
-    while (true) {
-        String texto = JOptionPane.showInputDialog("Digite sua matrícula: ");
+        while (true) {
+            String texto = JOptionPane.showInputDialog("Digite sua matrícula: ");
 
-        if (texto == null) {
-            return null;
-        }
+            if (texto == null) {
+                return null;
+            }
 
-        try {
-            int numeroMatricula = Integer.parseInt(texto);
-            return numeroMatricula;
+            try {
+                int numeroMatricula = Integer.parseInt(texto);
+                return numeroMatricula;
 
             } catch (NumberFormatException e) {
                 JOptionPane.showMessageDialog(null, "Matrícula inválida! Digite um número inteiro.",
@@ -121,7 +123,7 @@ public class Cadastro {
 
     private Double lerSalario() {
         String texto = "";
-        
+
         while (true) {
             texto = JOptionPane.showInputDialog("Digite seu salário (Ao invés de usar vírgula, utilize ponto ex: 1.5): ");
 
@@ -132,19 +134,60 @@ public class Cadastro {
             }
 
             try {
-            double salario = Double.parseDouble(texto);
+                double salario = Double.parseDouble(texto);
 
-            if (salario < 0) {
-                JOptionPane.showMessageDialog(null, "Salário inválido! Seu salário não pode ser negativo.");
-                continue;
-            }
+                if (salario < 0) {
+                    JOptionPane.showMessageDialog(null, "Salário inválido! Seu salário não pode ser negativo.");
+                    continue;
+                }
 
-            return salario;
-                
+                return salario;
+
             } catch (NumberFormatException e) {
                 JOptionPane.showMessageDialog(null, "Salário inválido. Digite um valor utilizando ponto ao invés de vírgula (ex: 1.5).");
                 continue;
             }
         }
+    }
+
+    private void cadastrarColaborador() {
+        String tipo = lerTipoColaborador();
+        if (tipo == null) {
+            return;
+        }
+
+        String nome = lerNome();
+        if (nome == null) {
+            return;
+        }
+
+        Integer matricula = lerMatricula();
+        if (matricula == null) {
+            return;
+        }
+
+        Double salario = lerSalario();
+        if (salario == null) {
+            return;
+        }
+
+        Colaborador novoColaborador = null;
+
+        if (tipo.equals("Padrão")) {
+            novoColaborador = new novoColaborador(nome, matricula, salario);
+
+        } else if (tipo.equals("Comissionado")) {
+
+        } else if (tipo.equals("Produção")) {
+
+        }
+
+        try {
+            gerenciador.adicionarColaborador(novoColaborador);
+            JOptionPane.showMessageDialog(null, "Colaboador cadastrado com sucesso");
+        } catch (IllegalArgumentException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+
     }
 }
